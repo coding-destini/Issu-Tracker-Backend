8730be93a6df42647573cd10064533e5fbdd416c
coding-destini <akashshahngu@gmail.com>
2023-05-20 13:50:49 +0530

    some update

--- routes/project.js
+++ routes/project.js
@@ -5,10 +5,10 @@ const passport = require('passport')
 
 router.post('/create',passport.authenticate('jwt',{session:false}),projectController.createProject); // because we already using json web token so we don't need to store
 // anything in session , that's why session is false 
-router.delete('/delete/:projectId',projectController.deleteproject);
-router.put('/update/:projectId',projectController.updateproject)
-router.get('/Allprojects',projectController.getProjects);
-router.get('/projectdetails/:projectId',projectController.getProjectDetails)
-router.post('/issue/:projectId',projectController.createIssue)
-router.get('/getIssues/:projectId',projectController.getIssues)
+router.delete('/delete/:projectId',passport.authenticate('jwt',{session:false}),projectController.deleteproject);
+router.put('/update/:projectId',passport.authenticate('jwt',{session:false}),projectController.updateproject)
+router.get('/Allprojects',passport.authenticate('jwt',{session:false}),projectController.getProjects);
+router.get('/projectdetails/:projectId',passport.authenticate('jwt',{session:false}),projectController.getProjectDetails)
+router.post('/issue/:projectId',passport.authenticate('jwt',{session:false}),projectController.createIssue)
+router.get('/getIssues/:projectId',passport.authenticate('jwt',{session:false}),projectController.getIssues)
 module.exports=router;
